cd587a63ed697975be60bd867c20214cadb203e6
FedeFleurent <fedefleurent89@gmail.com>
2022-05-19 22:46:04 -0300

    cambios varios

---
+++ src/Navbar/NavBar.js
@@ -0,0 +1,24 @@
+import "./Navbar.css"
+
+export const Navegacion =  () => { 
+     
+    return  (
+
+        <div className="navBarCointainer">
+
+             <h1 className="navBarLogo">Federico Fleurent</h1>
+
+            <ul className="navLinks">
+
+                <li>Home </li>
+
+                <li>Categoria 1 </li>
+
+                <li>Categortia 2</li>
+
+                <li>Categortia 3</li>
+            </ul>
+            
+        </div>
+    )
+}
